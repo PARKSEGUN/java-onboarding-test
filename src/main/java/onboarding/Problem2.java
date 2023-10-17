12d335822756d87c98d10b4f62113fd91f6e22e2
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-17 18:40:10 +0900

    [2번 문제] stackToString 메서드 구현 & 모든 테스트 통과 & 체크리스트 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -4,31 +4,12 @@ import java.util.Stack;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "";
-        System.out.println(pushAnswer(cryptogram));
-
+        String answer = pushAnswer(cryptogram);
         return answer;
-    }
-
-    private static String stackToString(Stack<Character> answerFromStack) {
-        String answer = "";
-        for (Character character : answerFromStack) {
-            answer += character.charValue();
-        }
-        return answer;
-    }
-
-
-    //top 부분과 들어올 문자를 반복해서 비교후 적절한 idx 리턴
-    public static int findFitIdx(char top, int i, String cryptogram) {
-        while (i<cryptogram.length()&&cryptogram.charAt(i) == top) {
-            i++;
-        }
-        return --i;
     }
 
     //스택을 사용해서 중복제거 후 answer 리턴
-    public static  Stack<Character> pushAnswer(String cryptogram) {
+    public static String pushAnswer(String cryptogram) {
         Stack<Character> answer = new Stack<>();
         answer.push(cryptogram.charAt(0));
         for (int i = 1; i < cryptogram.length(); i++) {
@@ -38,6 +19,23 @@ public class Problem2 {
                 continue;
             }
             answer.push(cryptogram.charAt(i));
+        }
+        return stackToString(answer);
+    }
+
+    //top 부분과 들어올 문자를 반복해서 비교후 적절한 idx 리턴
+    public static int findFitIdx(char top, int i, String cryptogram) {
+        while (i < cryptogram.length() && cryptogram.charAt(i) == top) {
+            i++;
+        }
+        return --i;
+    }
+
+    //스택을 문자열로 변환해주는 메서드
+    private static String stackToString(Stack<Character> answerFromStack) {
+        String answer = "";
+        for (Character character : answerFromStack) {
+            answer += character.charValue();
         }
         return answer;
     }
