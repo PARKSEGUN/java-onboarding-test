7bc9e1ad5399db06da7601afedc0362b7471a3f3
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-17 16:25:38 +0900

    [1번 문제] winnerResult 메서드 구현 & 예외사항 처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,16 +5,39 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        System.out.println(isPageNormal(pobi));
-        System.out.println(isPageNormal(crong));
+        try {
+            if(isPageNormal(pobi)==false||isPageNormal(crong)==false){
+                return -1;
+            }
+            int pobiMaxScore = findMaxByUser(pobi);
+            int crongMaxScore = findMaxByUser(crong);
+            answer= winnerResult(pobiMaxScore, crongMaxScore);
+        } catch (Exception e) {
+            return -1;
+        }
+
         return answer;
     }
 
+    //누가 이겼는지, 비겼는지에 대한 결과를 리턴하는 메서드
+    public static int winnerResult(int pobiMaxScore, int crongMaxScore) {
+        if (pobiMaxScore > crongMaxScore) {
+            return 1;
+        } else if (pobiMaxScore < crongMaxScore) {
+            return 2;
+        } else if (pobiMaxScore == crongMaxScore) {
+            return 0;
+        }
+        return -1;
+    }
+
+    //사용자의 책 페이지번호들로 최대의 점수를 리턴하는 메서드
     public static int findMaxByUser(List<Integer> pages) {
         int leftPageMax = findMaxByPage(pages.get(0));
         int rightPageMax= findMaxByPage(pages.get(1));
         return Math.max(leftPageMax, rightPageMax);
     }
+    //페이지번호로 만들 수 있는 최대 점수를 리턴하는 메서드
     public static int findMaxByPage(int page) {
         int PageAddScore=0;
         int PageMultiScore =1;
@@ -27,6 +50,7 @@ class Problem1 {
         System.out.println(PageMultiScore);
         return Math.max(PageAddScore, PageMultiScore);
     }
+    //주어진 페이지 번호들의 정보가 잘못된것은 없는지 확인해주는 메서드
     public static boolean isPageNormal(List<Integer> user) {
         int leftPage = user.get(0);
         int rightPage = user.get(1);
