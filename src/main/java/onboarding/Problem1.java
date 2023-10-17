55235570c259be04b999db3975c6c14a50b072ef
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-17 15:56:14 +0900

    [1번 문제] findMaxByPage 함수 구현, findMaxByUser 함수 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer2 = Integer.MAX_VALUE;
-        return answer2;
+        int answer = Integer.MAX_VALUE;
+        findMaxByUser(pobi);
+        return answer;
+    }
+
+    public static int findMaxByUser(List<Integer> pages) {
+        int leftPageMax = findMaxByPage(pages.get(0));
+        int rightPageMax= findMaxByPage(pages.get(1));
+        return Math.max(leftPageMax, rightPageMax);
+    }
+    public static int findMaxByPage(int page) {
+        int PageAddScore=0;
+        int PageMultiScore =1;
+        while(page>0){
+            PageAddScore+=page%10;
+            PageMultiScore *= page % 10;
+            page /= 10;
+        }
+        System.out.println(PageAddScore);
+        System.out.println(PageMultiScore);
+        return Math.max(PageAddScore, PageMultiScore);
     }
 }
