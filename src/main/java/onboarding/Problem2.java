1109a99efcf4d18dcf1933fd101595e77107f149
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-17 18:34:09 +0900

    [2번 문제] pushAnswer 메서드 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -18,31 +18,27 @@ public class Problem2 {
         return answer;
     }
 
+
     //top 부분과 들어올 문자를 반복해서 비교후 적절한 idx 리턴
-    public static int findfitIdx(char top, int i, String cryptogram) {
+    public static int findFitIdx(char top, int i, String cryptogram) {
         while (i<cryptogram.length()&&cryptogram.charAt(i) == top) {
             i++;
         }
         return --i;
     }
 
+    //스택을 사용해서 중복제거 후 answer 리턴
     public static  Stack<Character> pushAnswer(String cryptogram) {
         Stack<Character> answer = new Stack<>();
         answer.push(cryptogram.charAt(0));
-        char top = answer.lastElement();
         for (int i = 1; i < cryptogram.length(); i++) {
-            if (top == cryptogram.charAt(i)) {
-                i = findfitIdx(top, i, cryptogram);
+            if (answer.lastElement() == cryptogram.charAt(i)) {
+                i = findFitIdx(answer.lastElement(), i, cryptogram);
                 answer.pop();
-                if(answer.size()>0){
-                    top = answer.lastElement();
-                }
                 continue;
             }
             answer.push(cryptogram.charAt(i));
-            top = cryptogram.charAt(i);
         }
-
         return answer;
     }
 }
