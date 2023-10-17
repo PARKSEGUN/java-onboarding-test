c79a4a4fa81c54b2c2bddc65373e2029162bab05
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-17 21:17:10 +0900

    [3번 문제] countByNumber 메서드 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,9 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        //1 ~ number 까지 반복
+        for(int currentNumber=1;currentNumber<=number;currentNumber++){
+            answer += countByNumber(currentNumber);
+        }
         return answer;
     }
-
+    //리턴된 결과를 더해서 총 쳐야되는 손뼉 개수 리턴
+    private static int countByNumber(int curNumber) {
+        int countSum = 0;
+        while(curNumber>0){
+            countSum += isDivide3(curNumber%10);
+            curNumber /= 10;
+        }
+        return countSum;
+    }
 
     //3,6,9 가 몇개 포함되어있는지 리턴
     private static int isDivide3(int curNumber) {
