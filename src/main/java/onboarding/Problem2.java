ca34eeabfe66d37dd685491164d7f6c1b1b7c78e
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-17 20:19:01 +0900

    [2번 문제] pushAnswer 메서드 수정

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -12,23 +12,23 @@ public class Problem2 {
     public static String pushAnswer(String cryptogram) {
         Stack<Character> answer = new Stack<>();
         answer.push(cryptogram.charAt(0));
-        for (int i = 1; i < cryptogram.length(); i++) {
-            if (answer.lastElement() == cryptogram.charAt(i)) {
-                i = findFitIdx(answer.lastElement(), i, cryptogram);
+        for (int idx = 1; idx < cryptogram.length(); idx++) {
+            if (answer.lastElement() == cryptogram.charAt(idx)) {
+                idx = findFitIdx(answer.lastElement(), idx, cryptogram);
                 answer.pop();
                 continue;
             }
-            answer.push(cryptogram.charAt(i));
+            answer.push(cryptogram.charAt(idx));
         }
         return stackToString(answer);
     }
 
     //top 부분과 들어올 문자를 반복해서 비교후 적절한 idx 리턴
-    public static int findFitIdx(char top, int i, String cryptogram) {
-        while (i < cryptogram.length() && cryptogram.charAt(i) == top) {
-            i++;
+    public static int findFitIdx(char top, int idx, String cryptogram) {
+        while (idx < cryptogram.length() && cryptogram.charAt(idx) == top) {
+            idx++;
         }
-        return --i;
+        return --idx;
     }
 
     //스택을 문자열로 변환해주는 메서드
