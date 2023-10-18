f78f2338de7cde5076f3e6e55dbff6b4ade985cf
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-10-19 04:32:53 +0900

    fead(problem7):정렬을 위한 People클래스 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -2,109 +2,128 @@ package onboarding;
 
 import java.util.*;
 
-//객체를 정렬하기위한 compareTo 메서드를 사용하기위해 Comparable interface 를 상속받는다.
-//주어진 friends 정보에서 user의 친구를 뽑아낸다
-//다시 friends에서 뽑아낸친구들의 친구를 뽑아낸다.
-//이 친구들은 10점씩 추가해준다.
-//visitors 중 user의 친구가 아닌 사람을 확인
-//확인 한 친구들은 1점씩 추가
-//모든 점수를 더해주었을때에 가장 점수가 높은 5명 추출
-//점수가 같다면 이름을 기준으로 오름차순 정렬
-class People implements Comparable {
+class People implements Comparable<People> {
+    String name;
+    int score;
 
-    private String name;
-
-    private int score = 0;
-
-//    private List<People> friends = new ArrayList<>();
-
-    public People(String name) {
+    public People(String name, int score) {
         this.name = name;
+        this.score = score;
     }
 
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(People o) {
+        if (this.score == o.score) {
+            return this.name.compareTo(o.name);
+        }
+        return  o.score-this.score ;
     }
 
-
-//
-
-    public boolean isEqualName(String name) {
-        if (this.name.equals(name)) {
-            return true;
-        }
-        return false;
-    }
-//
-//    public void saveFriends(List<People> userFriends) {
-//        this.friends = userFriends;
-//    }
-
-    public Map<String, Integer> modifyScoreAdd10(Map<String, Integer> scoreByName, People people) {
-        if (scoreByName.containsKey(people.name)) {
-            int score = scoreByName.get(people.name).intValue();
-            scoreByName.put(people.name, score+10);
-        } else if (!scoreByName.containsKey(people.name)) {
-            scoreByName.put(people.name, 10);
-        }
-        return scoreByName;
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
     }
 }
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        People userByPeople = new People(user);
-        List<People> userFriends = new ArrayList<>();
-        List<People> friendsByUserFriends = new ArrayList<>();
-        Map<String, Integer> scoreByName = new HashMap<>();
 
-        userFriends = findFriends(userFriends, friends, userByPeople);
-        friendsByUserFriends = findFriendsByUserFriends(userFriends, friends, friendsByUserFriends);
-        for (People friendsByUserFriend : friendsByUserFriends) {
-            friendsByUserFriend.modifyScoreAdd10(scoreByName, friendsByUserFriend);
-        }
-        System.out.println(scoreByName);
-        
+    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        List<String> answer = new ArrayList<>();
+        List<String> userFriends = findFriends(friends, user);
+        List<String> friendsByUserFriends = findFriendsByUserFriends(friends, user, userFriends);
+        Map<String, Integer> scoreByName = makeMap(user, userFriends, friendsByUserFriends, visitors);
+        findAnswer(answer, scoreByName);
         return answer;
     }
 
-    private static List<People> findFriendsByUserFriends(List<People> userFriends, List<List<String>> friends, List<People> friendsByUserFriends) {
-        for (People userFriend : userFriends) {
-            friendsByUserFriends = findFriends(friendsByUserFriends, friends, userFriend);
+    private static void findAnswer(List<String> answer, Map<String, Integer> scoreByName) {
+
+        List<People> peopleList = mapToClass(scoreByName);
+
+        System.out.println(peopleList);
+        Collections.sort(peopleList);
+        System.out.println(peopleList);
+    }
+
+    private static List<People> mapToClass(Map<String, Integer> scoreByName) {
+        List<People> peopleList = new ArrayList<>();
+        for (String name : scoreByName.keySet()) {
+            peopleList.add(new People(name, scoreByName.get(name)));
+        }
+        return peopleList;
+    }
+
+    private static Map<String, Integer> makeMap(String user, List<String> userFriends, List<String> friendsByUserFriends, List<String> visitors) {
+        Map<String, Integer> scoreByName = putToMap(friendsByUserFriends);
+        deleteUserAndUserFriend(scoreByName, user, userFriends);
+        plusScoreByVisitors(scoreByName, visitors);
+        return scoreByName;
+    }
+
+    private static void plusScoreByVisitors(Map<String, Integer> scoreByName, List<String> visitors) {
+        for (String visitor : visitors) {
+            if (scoreByName.containsKey(visitor)) {
+                int score = scoreByName.get(visitor);
+                scoreByName.put(visitor, score + 1);
+            } else {  //else 없애보자
+                scoreByName.put(visitor, 1);
+            }
+        }
+    }
+
+    private static void deleteUserAndUserFriend(Map<String, Integer> scoreByName, String user, List<String> userFriends) {
+        scoreByName.remove(user);
+        for (String userFriend : userFriends) {
+            scoreByName.remove(userFriend);
+        }
+    }
+
+    private static Map<String, Integer> putToMap(List<String> friendsByUserFriends) {
+        Map<String, Integer> scoreByName = new HashMap<>();
+        for (String friendsByUserFriend : friendsByUserFriends) {
+            if (scoreByName.containsKey(friendsByUserFriend)) {
+                int score = scoreByName.get(friendsByUserFriend);
+                scoreByName.put(friendsByUserFriend, score + 10);
+            } else {  //else 없애보자
+                scoreByName.put(friendsByUserFriend, 10);
+            }
+        }
+        return scoreByName;
+    }
+
+    private static List<String> findFriendsByUserFriends(List<List<String>> friends, String person, List<String> userFriends) {
+        List<String> friendsByUserFriends = new ArrayList<>();
+        for (String userFriend : userFriends) {
+            friendsByUserFriends.addAll(findFriends(friends, userFriend));
         }
         return friendsByUserFriends;
+
     }
 
-
-    private static List<People> findFriends(List<People> userFriends, List<List<String>> friends, People people) {
+    private static List<String> findFriends(List<List<String>> friends, String person) {
+        List<String> userFriends = new ArrayList<>();
         for (List<String> friend : friends) {
-            String name1 = friend.get(0);
-            String name2 = friend.get(1);
-            String userFriend = findFriend(name1, name2, people);
-            userFriends = addUserFriend(userFriends, userFriend);
+            //메서드를 나눌 수 있을까
+            String friend1 = friend.get(0);
+            String friend2 = friend.get(1);
+            String friendByName = findFriendByName(friend1, friend2, person);
+            if (!friendByName.equals("")) {
+                userFriends.add(friendByName);
+            }
         }
         return userFriends;
     }
 
-    private static List<People> addUserFriend(List<People> userFriends, String userFriend) {
-        if (userFriend.equals("")) {
-            return userFriends;
-        }
-        People friend = new People(userFriend);
-        userFriends.add(friend);
-        return userFriends;
-    }
-
-    private static String findFriend(String name1, String name2, People people) {
-        if (people.isEqualName(name1)) {
-            return name2;
-        } else if (people.isEqualName(name2)) {
-            return name1;
+    private static String findFriendByName(String friend1, String friend2, String person) {
+        if (friend1.equals(person)) {
+            return friend2;
+        } else if (friend2.equals(person)) {
+            return friend1;
         }
         return "";
     }
-
-
 }
+
